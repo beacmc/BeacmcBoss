5d790fbd74eb1107e320585d0fcfdfa540c90bed
beacmc <kornieiko09@gmail.com>
2024-05-16 18:56:52 +0300

    add 2 types requirement

---
+++ src/main/java/com/beacmc/beacmcboss/util/requirement/BooleanRequirement.java
@@ -0,0 +1,34 @@
+package com.beacmc.beacmcboss.util.requirement;
+
+import com.beacmc.beacmcboss.api.requirement.Requirement;
+import com.beacmc.beacmcboss.boss.Boss;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
+
+public class BooleanRequirement extends Requirement {
+
+
+    @Override
+    public String getName() {
+        return "[boolean]";
+    }
+
+    @Override
+    public String getDescription() {
+        return "checks whether the message outputs yes or true";
+    }
+
+    @Override
+    public boolean execute(Player player, Boss boss, String param) {
+        final String msg = PlaceholderAPI.setPlaceholders(player, param);
+        if(msg.isEmpty())
+            return false;
+
+        if(msg.contains("true") || msg.contains("yes")) {
+            return !msg.startsWith("!");
+        } else if (msg.contains("false") || msg.contains("no")) {
+            return msg.startsWith("!");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/beacmc/beacmcboss/util/requirement/JavascriptRequirement.java
@@ -0,0 +1,51 @@
+package com.beacmc.beacmcboss.util.requirement;
+
+import com.beacmc.beacmcboss.api.requirement.Requirement;
+import com.beacmc.beacmcboss.boss.Boss;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavascriptRequirement extends Requirement {
+    @Override
+    public String getName() {
+        return "[javascript]";
+    }
+
+    @Override
+    public String getDescription() {
+        return "copy of checks from JS";
+    }
+
+    @Override
+    public boolean execute(Player player, Boss boss, String param) {
+        final String msg = PlaceholderAPI.setPlaceholders(player, param);
+        if(msg.isEmpty())
+            return false;
+
+        final Pattern pattern = Pattern.compile("(.+?)(==|!=|<=|>=|<|>)(.+)");
+        final Matcher matcher = pattern.matcher(msg);
+
+        if(!matcher.matches())
+            return false;
+
+        final String first = matcher.group(1).trim();
+        final String operator = matcher.group(2);
+        final String second = matcher.group(3).trim();
+
+        try {
+            return switch (operator) {
+                case "==" -> first.equals(second);
+                case "!=" -> !first.equals(second);
+                case "<" -> Integer.parseInt(first) < Integer.parseInt(second);
+                case ">" -> Integer.parseInt(first) > Integer.parseInt(second);
+                case "<=" -> Integer.parseInt(first) <= Integer.parseInt(second);
+                case ">=" -> Integer.parseInt(first) >= Integer.parseInt(second);
+                default -> false;
+            };
+        } catch (NumberFormatException e) { }
+        return false;
+    }
+}
