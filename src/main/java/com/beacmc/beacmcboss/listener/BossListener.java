898633fbd5fa0040aa25474cd7f8c4f5266ae9cd
beacmc <kornieiko09@gmail.com>
2024-05-25 21:09:14 +0300

    add boss listener

---
+++ src/main/java/com/beacmc/beacmcboss/listener/BossListener.java
@@ -0,0 +1,71 @@
+package com.beacmc.beacmcboss.listener;
+
+import com.beacmc.beacmcboss.BeacmcBoss;
+import com.beacmc.beacmcboss.api.event.BossDeathEvent;
+import com.beacmc.beacmcboss.api.trigger.TriggerManager;
+import com.beacmc.beacmcboss.api.trigger.TriggerType;
+import com.beacmc.beacmcboss.boss.Boss;
+import com.beacmc.beacmcboss.boss.manager.BossManager;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+import java.io.File;
+import java.io.IOException;
+
+public class BossListener implements Listener {
+
+    private final BossManager bossManager;
+    private final TriggerManager triggerManager;
+
+    public BossListener() {
+        bossManager = BeacmcBoss.getBossManager();
+        triggerManager = BeacmcBoss.getTriggerManager();
+    }
+
+    @EventHandler
+    public void onBossDeath(EntityDeathEvent event) {
+        final LivingEntity entity = event.getEntity();
+        final YamlConfiguration data = BeacmcBoss.getDataConfig();
+        final Boss boss = bossManager.getBossByEntity(entity);
+        final Player killer = event.getEntity().getKiller();
+
+        if(boss != null) {
+            BossDeathEvent bossDeathEvent = new BossDeathEvent(boss, killer);
+            bossDeathEvent.callEvent();
+
+            if(bossDeathEvent.isCancelled()) {
+                event.setCancelled(true);
+                return;
+            }
+
+            try {
+                File file = new File(BeacmcBoss.getInstance().getDataFolder(), "data.yml");
+                data.load(file);
+                data.set("bosses." + boss.getName() + ".last-killer", killer.getName());
+                data.save(file);
+            } catch (IOException | InvalidConfigurationException e) { }
+
+            triggerManager.executeTriggers(killer, boss, TriggerType.BOSS_DEATH);
+            boss.setLivingEntity(null);
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if(!(event.getDamager() instanceof LivingEntity))
+            return;
+
+        final LivingEntity entity = (LivingEntity) event.getDamager();
+        final Boss boss = bossManager.getBossByEntity(entity);
+
+        if(boss != null) {
+            event.setDamage(boss.getCustomDamage());
+        }
+    }
+}
